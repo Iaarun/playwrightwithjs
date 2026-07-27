ab84c56cffa32c4dd4fc5c97d11f9a3279e6f7fe
Iaarun <automationbyaarun@gmail.com>
2026-07-27 09:01:23 +0530

    adding arrays and string

--- repeatsession/_0007Arrays.js
+++ repeatsession/_0007Arrays.js
@@ -1,6 +1,5 @@
 //Arrays  , these are index based 
 // array declaration using literals
-
 const fruits = ['apples', 'banana', 'papaya']
 console.log(typeof fruits)
 console.log("Length of fruits array: "+fruits.length)
@@ -11,11 +10,54 @@ console.log(fruits[1])
  console.log(typeof vegies)
  console.log("Length of vegies array: "+vegies.length)
 
- vegies.push("patato") // push function always adds data in the end of the array
+ vegies.push("potato") // push function always adds data in the end of the array
 vegies.push("tomato")
 vegies.unshift('onion') // add data in the begining
-vegies[4] = 'pumpkin'
- console.log(vegies)
- vegies.pop() // remove data from end
- vegies.shift() // remove data from the begining
-  console.log(vegies)
+vegies[3] = 'pumpkin'
+ //console.log(vegies)
+// vegies.pop() // remove data from end
+// vegies.shift() // remove data from the begining
+console.log(vegies)
+vegies.sort() // sort data in ascending order
+console.log(vegies)
+
+const reversearr= vegies.toReversed()
+console.log(reversearr)
+
+//slice - creata shallow copy of portion or array
+
+console.log(vegies.slice(2))
+// index of 
+console.log(vegies.indexOf('pumpkin12'))
+
+vegies.push('onion')
+console.log(vegies)
+console.log(vegies.indexOf('potato'))
+console.log(vegies.lastIndexOf('potato'))
+
+//filter
+let result= vegies.filter((veg)=> veg.length >=7)
+console.log(result)
+// iterate over array
+console.log('****** using for loop******')
+for(let i=0; i<=vegies.length-1; i++){
+    console.log(vegies[i])
+}
+
+console.log('****** using for in loop (to get indexes)******')
+for( let data in vegies){
+    console.log(data+": "+vegies[data])
+}
+
+console.log('****** using for of loop (to get values)******')
+for( let data of vegies){
+    console.log(data)
+}
+
+console.log(vegies[1])
+
+console.log(fruits)
+console.log(vegies)
+//let newarr= fruits.concat(vegies)
+//console.log(newarr)
+console.log(...fruits, ...vegies)
---
+++ repeatsession/_0008string.js
@@ -0,0 +1,4 @@
+let name = 'js'
+name = "Arrays"
+console.log(`${name} is 'js' provided`)
+console.log(typeof name)
